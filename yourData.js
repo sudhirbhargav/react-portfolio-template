bd455250a33552fd418e73018622652c38155c1e
Sudhir Bhargav <93834423+sudhirbhargav@users.noreply.github.com>
2022-06-04 04:34:28 +0530

    Add files via upload

---
+++ yourData.js
@@ -0,0 +1,123 @@
+export default {
+  //(Please Do Not Remove The comma(,) after every variable)
+  //Change The Website Template
+
+  //   Header Details ---------------------
+  name: "Sudhir bhargav",
+  headerTaglineOne: "Hello 👋",
+  headerTaglineTwo:
+    "I'm Sudhir Bhargav - I build things for the web. ",
+
+  socials: [
+    { title: "Github", link: "https://github.com/sudhirbhargav" },
+    { title: "LinkedIn", link: "https://www.linkedin.com/in/sudhir-bhargav/" },
+    { title: "Twitter", link: "https://twitter.com/BotSudhir" },
+    // { title: "Blog", link: "https://blog.chetanverma.com/" },
+    { title: "Email", link: "mailto:sudhirbhargav100@gmail.com" },
+  ],
+
+  // End Header Details -----------------------
+
+  // Work Section ------------------------
+  projects: [
+    {
+      title: "The Outnet", //Project Title - Add Your Project Title Here
+      description: "THE OUTNET is a company operating as a fashion retailer.",
+      //Project Image - Add Your Project Image Here
+      imageSrc:"https://i.postimg.cc/4yj9j9bn/Workpic.png",
+      //Project URL - Add Your Project Url Here
+      url: "https://github.com/sudhirbhargav/TheOutNet",
+    },
+    {
+      title: "The Home Depot", //Project Title - Add Your Project Title Here
+      description: "The Home Depot, is the home improvement retailer in the United States",
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://i.postimg.cc/CxGq6dZL/thehome.png",
+      //Project URL - Add Your Project Url Here
+      url: "https://github.com/sudhirbhargav/Unit3_construct_week",
+    },
+    {
+      title: "Project Three", //Project Title - Add Your Project Title Here
+      description: "Design",
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://i.postimg.cc/Xvxy4hcR/homes-Page.png",
+      //Project URL - Add Your Project Url Here
+      url: "http://chetanverma.com/",
+    },
+    // {
+    //   title: "Project Four", //Project Title - Add Your Project Title Here
+    //   description: "Marketing",
+    //   //Project Image - Add Your Project Image Here
+    //   imageSrc:
+    //     "https://images.unsplash.com/photo-1492171983775-a51717616c0d?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8NjB8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
+    //   //Project URL - Add Your Project Url Here
+    //   url: "http://chetanverma.com/",
+    // },
+    // {
+    //   title: "Project Five", //Project Title - Add Your Project Title Here
+    //   description: "Development",
+    //   //Project Image - Add Your Project Image Here
+    //   imageSrc:
+    //     "https://images.unsplash.com/photo-1534239143101-1b1c627395c5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8NzZ8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
+    //   //Project URL - Add Your Project Url Here
+    //   url: "http://chetanverma.com/",
+    // },
+    // {
+    //   title: "Project Six", //Project Title - Add Your Project Title Here
+    //   description: "Design & Development",
+    //   //Project Image - Add Your Project Image Here
+    //   imageSrc:
+    //     "https://images.unsplash.com/photo-1517479149777-5f3b1511d5ad?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTAyfHxwYXN0ZWx8ZW58MHx8MHw%3D&auto=format&fit=crop&w=400&q=60",
+    //   //Project URL - Add Your Project Url Here
+    //   url: "http://chetanverma.com/",
+    // },
+
+    /*
+    If You Want To Add More Project just Copy and Paste This At The End
+    ,{
+        title: 'Project Five',
+        description: 'Something Amazing',
+        imageSrc: "",
+        url: ''
+    }
+    */
+  ],
+
+  // End Work Section -----------------------
+
+  // Services Section ---------------
+
+  // services: [
+  //   {
+  //     title: "Art Direction",
+  //     description:
+  //       "We help with the creation and development of online advertising ideas, with particular focus on their visual appearance.",
+  //   },
+  //   {
+  //     title: "Branding",
+  //     description:
+  //       "We design key brand elements such as the logo, color scheme, typography, and other design components that makes your brand stand out from competitors.",
+  //   },
+  //   {
+  //     title: "Web Design",
+  //     description:
+  //       "We build and optimize your online presence.  Website is the digital entry point into your business and a powerful revenue channel.",
+  //   },
+  //   {
+  //     title: "3D Design",
+  //     description:
+  //       "We combine creative design and technical skills to build striking 3D visualisations that bring your project to life.",
+  //   },
+  // ],
+
+  // End Services Section --------------------------
+
+  // About Secton --------------
+  aboutpara:
+    "Hi Everyone! I am Mohak Tripathi. I always believe that direction is far more important than speed. Keeping that in mind and understanding my inner self and where the world is heading, I decided to make a pivot in my career to become a software developerI love to be involved with technology and I have a desire to build web applications that can impact millions of life positively. . At Masai School in the last 7 months, I built a number of web applications using the MERN stack and developed key projects in it. I have also inculcated the skills like teamwork, leadership, empathy, compassion, and communication. After months of rigorous training, here I am looking for an opportunity as a full stack web developer.",
+  //   End About Section ---------------------
+};
+
+// Thanks for using this template, I would love to hear from you contact me at hello@chetanverma.com
